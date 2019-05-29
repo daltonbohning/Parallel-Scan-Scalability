70be9b70f02f1ea83687385c01abb72139cd397a
Jordan Kremer <jkrem@wino.cs.pdx.edu>
2019-05-29 13:24:16 -0700

    Basic algorithm implemented, still has issues

--- brent-kung.cu
+++ brent-kung.cu
@@ -1,4 +1,30 @@
 
+/*
+brent-kung.cu
+
+Jordan Kremer
+Dalton Bohning
+
+Usage:
+    Flags:
+        -DARRAY_SIZE
+        -SECTION_SIZE 
+
+    Ex:
+        nvcc -DARRAY_SIZE=2000 -DSECTION_SIZE=2048 -o brent-kung brent-kung.cu
+
+    Note:
+        Section size should not exceed 2048 
+*/
+
+
+#include <cuda.h>
+#include <stdio.h>
+
+
+//#define SECTION_SIZE 100
+//#define ARRAY_SIZE 100
+
 
 #define handleError(CUDA_FUNCTION) {\
     cudaError_t THE_ERROR = (cudaError_t) CUDA_FUNCTION;\
@@ -8,6 +34,7 @@
         exit(EXIT_FAILURE);\
     }\
 }
+
 
 
 //Credit: https://github.com/aramadia/udacity-cs344/blob/master/Unit2%20Code%20Snippets/gputimer.h
@@ -50,22 +77,24 @@ struct GpuTimer
 
 
 
-__global__ void Brent_Kung_scan_kernel(float *X, float *Y, int InputSize) {
-
+__global__ 
+void Brent_Kung_scan_kernel(float *X, float *Y)
+{
     __shared__ float XY[SECTION_SIZE];
+
     int i = 2*blockIdx.x*blockDim.x + threadIdx.x;
-    if(i < InputSize)
+    if(i < ARRAY_SIZE)
     {
         XY[threadIdx.x] = X[i];
     }
 
-    if(i + blockDim.x < InputSize)
+    if(i + blockDim.x < ARRAY_SIZE)
     {
-        XY(threadIdx.x+blockDim.x) = X[i + blockDim.x];
+        XY[threadIdx.x+blockDim.x] = X[i + blockDim.x];
     }
 
     for (unsigned int stride = 1; stride <= blockDim.x; stride *= 2) {
-        __synchthreads();
+        __syncthreads();
         int index = (threadIdx.x+1) * 2 * stride - 1;
         if(index < SECTION_SIZE)
         {
@@ -75,7 +104,7 @@ __global__ void Brent_Kung_scan_kernel(float *X, float *Y, int InputSize) {
 
     for(int stride = SECTION_SIZE/4; stride > 0; stride /= 2)
     {
-        __synchthreads();
+        __syncthreads();
         int index = (threadIdx.x+1) * stride * 2 - 1;
         if(index + stride < SECTION_SIZE) 
         {
@@ -83,14 +112,63 @@ __global__ void Brent_Kung_scan_kernel(float *X, float *Y, int InputSize) {
         }
     }
 
-    __synchthreads();
-    if(i < InputSize)
+    __syncthreads();
+    if(i < ARRAY_SIZE)
     {
         Y[i] = XY[threadIdx.x];
     }
     
-    if(i + blockDim.x < InputSize)
+    if(i + blockDim.x < ARRAY_SIZE)
     {
         Y[i + blockDim.x] = XY[threadIdx.x + blockDim.x];
+    }
+
+}
+
+
+void inclusive_scan(float *host_X, float *host_Y)
+{
+    float *X, *Y;
+
+    handleError(cudaMalloc((void **)&X, ARRAY_SIZE));
+    handleError(cudaMalloc((void **)&Y, ARRAY_SIZE));
+
+    handleError(cudaMemcpy(X, host_X, ARRAY_SIZE, cudaMemcpyHostToDevice));
+   
+    //Book says SECTION_SIZE/2 OK, but not sure about
+    //other dimensions and blocks per grid
+    dim3 threadsPerBlock(SECTION_SIZE/2, 1, 1);
+    dim3 blocksPerGrid(100,1,1);
+
+    GpuTimer timer;
+    timer.Start();
+    Brent_Kung_scan_kernel<<<blocksPerGrid, threadsPerBlock>>>(X, Y);
+    timer.Stop();
+
+    handleError(cudaMemcpy(host_Y, Y, ARRAY_SIZE, cudaMemcpyDeviceToHost));
+    handleError(cudaFree(X));
+    handleError(cudaFree(Y));
+
+}
+
+
+int main(void)
+{
+    float *host_X = (float*)malloc(ARRAY_SIZE);
+    float *host_Y = (float*)malloc(ARRAY_SIZE);
+
+    for(int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        host_X[i] = i + i %4; //change
+    }
+
+    inclusive_scan(host_X, host_Y);
+
+    for(int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        printf("%f", host_Y[i]);
+        if(i % 10 == 0){
+            printf("\n");
+        }
     }
 }
